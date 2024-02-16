34f952ca14493d7fec8e4b543c99f6644de6ab5c
Ian Lai <lailai820201@gmail.com>
2024-02-16 18:56:12 +0800

    chore: output every function folder

---
+++ rollup.config.ts
@@ -0,0 +1,65 @@
+import fs from 'node:fs';
+
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
+import { RollupOptions } from 'rollup';
+import dts from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
+
+function getFiles(path: string) {
+  const functionNames: string[] = [];
+
+  fs.readdirSync(path).forEach((file) => {
+    functionNames.push(`${file}`);
+  });
+
+  return functionNames;
+}
+
+const configs: RollupOptions[] = [];
+
+getFiles('src').forEach((fn) => {
+  const fnName = fn === 'index.ts' ? `index.ts` : `${fn}/index.ts`;
+  const input = `src/${fnName}`;
+
+  configs.push({
+    input,
+    plugins: [resolve(), commonjs(), esbuild()],
+    output: [
+      {
+        file: `dist/${fnName.replace('.ts', '.js')}`,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: `dist/${fnName.replace('.ts', '.mjs')}`,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    external: ['lodash-es'],
+  });
+
+  // dts
+  configs.push({
+    input,
+    plugins: [dts()],
+    output: [
+      {
+        file: `dist/${fnName.replace('.ts', '.d.mts')}`,
+        format: 'esm',
+      },
+      {
+        file: `dist/${fnName.replace('.ts', '.d.ts')}`,
+        format: 'esm',
+      },
+      {
+        file: `dist/${fnName.replace('.ts', '.d.cts')}`,
+        format: 'cjs',
+      },
+    ],
+    external: [],
+  });
+});
+
+export default configs;
